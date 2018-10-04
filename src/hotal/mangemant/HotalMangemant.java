4fac8595c1324f419c985cf50331a4f23a1f8ee1
[rami Dannah] <[killx51@hotmail.com]>
2018-10-05 02:21:51 +0300

    Final

---
+++ src/hotal/mangemant/HotalMangemant.java
@@ -0,0 +1,12 @@
+package hotal.mangemant;
+public class HotalMangemant {
+    public static void main(String[] args) {
+       //Varables
+       int a = 46;
+       int b = 79;
+       int c = 20;
+       // the prints
+       System.out.print("___________________________________________________ \n"+"_____________ Hotal Mangemant System ______________ \n"+"Wellcome to our Hotal Mangemant System, we can hlep you to mange your hotal. \n" + "How many floors in your hotal? " + 7 + "\n" + "How many room in etch floor? " + 12 + "\n" + "How much room cost per day? " + 350 +"\n"+"how many reserved rooms in: "+ "\n" + "    Tursday  :    " + a + "\n" + "    Friday   :    "+ b +"\n" +"    Saturday   :  "+ c +"\n"+"___________________________________________________________" + "\n" +"     your hotal report is here " + "\n"+ "Day        " + "Reserved rooms     " + "non-reserved rooms     "  + "income    "+ "Occupancy" +"\n"+ "Tursday      " +"     "+ a + "            38      " + "             16100      " + "  54.76" +"\n"+ "Friday      "  +"      "+b+ "            5      " + "              27650       "+ " 94.05"+"\n"+"Saturday    "+"      "+c+"            64      " + "             7000       " +"  23.81"+ "\n" +"_____________________________________________________________________________"+"\n"+ "total             " + (a+b+c ) +"           " + (38+5+64) +"                  "+ (16100+27650+7000) +"        "+(54.76-94.05-23.81)+"\n"+ "\n Thank you for using our service, good bye ..."+"\n"+"          Print by Engineer rami ^_^ \n"); 
+
+}
+}
